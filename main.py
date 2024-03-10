f3f5dde8115458eb47ab2d4bc6cb4776b123e7a6
sateesh-py <134619896+sateesh-py@users.noreply.github.com>
2024-03-10 14:51:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import uvicorn
 from datagen import generate_data
 
 app = FastAPI()
+
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
 
 
 @app.get('/v2/random-data')
